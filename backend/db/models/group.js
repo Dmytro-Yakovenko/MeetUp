d211b93a28905fc80799d3d9f6cbd23b80ee54d8
Dmytro-Yakovenko <d.yakovenko1986@gmail.com>
2023-01-18 21:19:46 -0800

    get groups

---
+++ backend/db/models/group.js
@@ -0,0 +1,65 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Group extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Group.belongsTo(models.User,{
+        foreignKey:'organizerId'
+      })
+    }
+  }
+  Group.init({
+    name:{
+     type: DataTypes.STRING,
+     allowNull:false
+    } ,
+    
+    organizerId:{
+      type:DataTypes.INTEGER,
+      allowNull:false
+    } ,
+    about:{
+      type: DataTypes.STRING,
+     allowNull:false 
+    },
+    type:{
+      type: DataTypes.STRING,
+      allowNull:false
+    } ,
+    private:{
+      type: DataTypes.BOOLEAN,
+      allowNull:true
+    } ,
+    city:{
+      type: DataTypes.STRING,
+      allowNull:false
+    } ,
+
+    state:{
+      type: DataTypes.STRING,
+      allowNull:false
+    } ,
+    numMembers:{
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    previewImage:{
+      type: DataTypes.STRING,
+      allowNull:false
+    } 
+  }, {
+    sequelize,
+    modelName: 'Group',
+
+
+  });
+  return Group;
+};
