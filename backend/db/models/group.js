cf18a4fdf1b302e92f99b87aa1d60a8e65465827
Dmytro-Yakovenko <d.yakovenko1986@gmail.com>
2023-01-18 21:36:32 -0800

    groupimages

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -11,6 +11,14 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+
+      Group.hasMany(models.GroupImages, {
+        foreignKey:'allGroupsId',
+        onDelete:'Cascade',
+        hooks:true
+      })
+
+
       Group.belongsTo(models.User,{
         foreignKey:'organizerId'
       })
