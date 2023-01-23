886150d6de6f832f2a7791acae04e8aa6fd1d456
Dmytro-Yakovenko <d.yakovenko1986@gmail.com>
2023-01-22 21:18:33 -0800

    attendees

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -13,6 +13,11 @@ module.exports = (sequelize, DataTypes) => {
 
     static associate(models) {
       // define association here
+      User.belongsToMany(models.Event,{
+        through:models.Attendees
+      })
+
+
       User.belongsToMany(models.Group,{
         through:models.Membership
       })
