1a686b0e2ce5506f62cd0f233a751a093948ab22
Dmytro-Yakovenko <d.yakovenko1986@gmail.com>
2023-01-28 13:25:03 -0800

    changes

--- backend/db/migrations/20230119045343-create-group.js
+++ backend/db/migrations/20230119045343-create-group.js
@@ -1,8 +1,11 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Groups', {
+    return queryInterface.createTable('Groups', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -48,9 +51,9 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue:Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Groups');
+    await queryInterface.dropTable('Groups', options);
   }
 };
