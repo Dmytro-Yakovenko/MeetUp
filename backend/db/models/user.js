3ecde55b6bb1b3e8f1ef86cb720caeec070c4d08
Dmytro-Yakovenko <d.yakovenko1986@gmail.com>
2023-01-28 16:21:49 -0800

    debugging

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -68,11 +68,11 @@ module.exports = (sequelize, DataTypes) => {
     {
       firstName:{
         type: DataTypes.STRING,
-        allowNull:false
+        allowNull:true
       },
       lastName:{
         type: DataTypes.STRING,
-        allowNull:false
+        allowNull:true
       },
 
       username: {
