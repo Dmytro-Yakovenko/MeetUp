519b8bcec85978b12f3ec7b781395bebe5bd9b6c
Dmytro-Yakovenko <d.yakovenko1986@gmail.com>
2023-01-30 10:29:20 -0800

    delete

--- backend/db/seeders/20230122172007-eventImages.js
+++ backend/db/seeders/20230122172007-eventImages.js
@@ -7,42 +7,42 @@ if (process.env.NODE_ENV === 'production') {
 const eventImages = [
   {
     eventId:1,
-    preview:true,
+    
     url:"https://res.cloudinary.com/dr1ekjmf4/image/upload/v1674409076/pokerEventImages/istockphoto-1410017313-1024x1024_dstt1d.webp"
   },
   {
     eventId:1,
-    preview:true,
+   
     url:"https://res.cloudinary.com/dr1ekjmf4/image/upload/v1674409058/pokerEventImages/tyrell-james-3h8KD8l_Q0A-unsplash_ukummt.jpg"
   },
   {
     eventId:2,
-    preview:false,
+   
     url:"https://res.cloudinary.com/dr1ekjmf4/image/upload/v1674409044/pokerEventImages/istockphoto-505800420-612x612_qd2kfk.jpg"
   },
   {
     eventId:2,
-    preview:true,
+   
     url:"https://res.cloudinary.com/dr1ekjmf4/image/upload/v1674409032/pokerEventImages/istockphoto-619397934-1024x1024_h675cn.webp"
   },
   {
     eventId:3,
-    preview:true,
+    
     url:"https://res.cloudinary.com/dr1ekjmf4/image/upload/v1674409566/pokerEventImages/istockphoto-162329443-170x170_amr36n.jpg"
   },
   {
     eventId:3,
-    preview:true,
+    
     url:"https://res.cloudinary.com/dr1ekjmf4/image/upload/v1674409511/pokerEventImages/istockphoto-915607656-612x612_g0vrtl.jpg"
   },
   {
     eventId:4,
-    preview:true,
+    
     url:"https://res.cloudinary.com/dr1ekjmf4/image/upload/v1674409504/pokerEventImages/istockphoto-914839102-612x612_csw0gr.jpg"
   },
   {
     eventId:4,
-    preview:false,
+    
     url:"https://res.cloudinary.com/dr1ekjmf4/image/upload/v1674409494/pokerEventImages/istockphoto-508470473-612x612_o2wijc.jpg"
   }
 ]
