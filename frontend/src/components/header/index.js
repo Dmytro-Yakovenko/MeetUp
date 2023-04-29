da1b09be06222229d412f31960d4d44d32628515
Dmytro-Yakovenko <d.yakovenko1986@gmail.com>
2023-04-29 12:12:53 -0700

    log in

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -1,22 +1,35 @@
 import { NavLink } from "react-router-dom"
 
 
-function Header(){
+function Header({ isLoaded }) {
     return (
         <header>
             <NavLink to="/">
-            <h1>MEET UP</h1>
+                <h1>MEET UP</h1>
             </NavLink>
-           <div>
-        <NavLink to="/login">
-        Log In
-        </NavLink>
-        <NavLink to="/signup">
-        Sign Up
-        </NavLink>
-           
-           </div>
-        
+            <div>
+                {!isLoaded &&
+                    (
+                        <>
+                            <NavLink to="/login">
+                                Log In
+                            </NavLink>
+                            <NavLink to="/signup">
+                                Sign Up
+                            </NavLink>
+                        </>
+                    )
+
+                }
+{
+    isLoaded && 
+    <NavLink to="/logout">
+    Log Out
+    </NavLink>
+}
+
+            </div>
+
         </header>
     )
 }
